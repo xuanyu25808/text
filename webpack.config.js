505ef43491ef2b9293d874e46fcb32a6f08819bf
xuanyu <374508747@qq.com>
2021-02-08 20:20:31 +0800

    111

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 获取绝对路径
 const resolve = dir => path.resolve(__dirname, dir);
 
-/////////////////////
 
 module.exports = {
   mode: 'development',
